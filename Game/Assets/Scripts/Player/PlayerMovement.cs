e1c13d4f51498c98a772aad41bda965dcfa559d8
Rafael Canhoto <rafgoncan99@gmail.com>
2025-05-04 04:53:50 +0100

    Delete unecessery things in the project, remake the playermovement, make some adjusts in the project and add mud and textTimer script

--- Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,55 +1,149 @@
-using UnityEditor.Callbacks;
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UIElements;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Vector2 velocity;
+    [SerializeField] private string horizontalAxisName = "Horizontal";
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundCheckLayers;
+    [SerializeField, Range(0.1f, 5.0f)] private float groundCheckRadius = 2.0f;
+    [SerializeField, Header("Jump Settings")] private float jumpMaxDuration = 0.1f;
+    [SerializeField] private float jumpGravityScale = 1.0f;
+    [SerializeField, Header("Player Visuals")] private Animator animator;
+    [SerializeField] private Collider2D groundCollider;
+    [SerializeField] private Collider2D airCollider;
 
-    [SerializeField] private string horizontalInputName = "Horizontal";
-
-    [SerializeField] private float gravity = 0;
-
-    private Rigidbody2D rb;
+    private Rigidbody2D rigidBodyPlayer;
+    private SpriteRenderer playerRenderer;
+    private Quaternion initialRotation;
+    private bool isGround;
+    private float jumpTimer;
+    private float originalGravity;
     
-    private Animator _animator;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        rb = GetComponent<Rigidbody2D>();
-
-        _animator = GetComponent<Animator>();
+        rigidBodyPlayer = GetComponent<Rigidbody2D>();
+        playerRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        initialRotation = transform.rotation;
+        originalGravity = rigidBodyPlayer.gravityScale;
     }
-
-
+    // Update is called once per frame
     void Update()
     {
-        float MovHorizontal = Input.GetAxis(horizontalInputName);
-         
-        rb.linearVelocity = new Vector2(MovHorizontal * velocity.x, rb.linearVelocity.y - gravity * Time.deltaTime);
+        Grounded();
+        groundCollider.enabled = isGround;
+        airCollider.enabled = !isGround;
+        VisualsUpdate();
+        Jump();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    // Detect if the player is on the ground
+    void Grounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundCheckLayers);
+
+        // Check if the player is on the ground by checking for collisions with the ground layer+
+        if (collider != null)
         {
-            rb.AddForce(new Vector2(rb.linearVelocity.x, velocity.y), ForceMode2D.Impulse);
-        }
-      
-        if (MovHorizontal >= 0.1f)
-        {
-            _animator.SetBool("IsWalking", true);
-            _animator.SetBool("IsIdle", false);
-        }
-        else if (MovHorizontal <= -0.1f)
-        {
-            _animator.SetBool("IsWalkingB", true);
-            _animator.SetBool("IsIdle", false);
+            isGround = true;
         }
         else
         {
-            _animator.SetBool("IsWalking", false);
-            _animator.SetBool("IsWalkingB", false);
-            _animator.SetBool("IsIdle", true);
+            isGround = false;
+        }
+    }
+
+    // Update the player visuals based on the current state and the player's movement
+    void VisualsUpdate()
+    {
+        float playerMovHorizontal = Input.GetAxis(horizontalAxisName);
+
+        Vector2 currentVelocity = rigidBodyPlayer.linearVelocity;
+        currentVelocity.x = playerMovHorizontal * velocity.x;
+
+        // Move the player horizontally
+        if ((playerMovHorizontal < 0) && (transform.right.x > 0))
+        {
+            transform.rotation = initialRotation * Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+        else if ((playerMovHorizontal > 0) && (transform.right.x < 0))
+        {
+            transform.rotation = initialRotation;
         }
 
+        // Set the animator parameters
+        animator.SetFloat("AbsVelocityX", Mathf.Abs(currentVelocity.x));
+        animator.SetFloat("VelocityY", currentVelocity.y);
+        animator.SetBool("IsGrounded", isGround);
+    }
+
+    // Jumping logic
+    void Jump()
+    {
+        float playerMovHorizontal = Input.GetAxis(horizontalAxisName);
+        Vector2 currentVelocity = rigidBodyPlayer.linearVelocity;
+        currentVelocity.x = playerMovHorizontal * velocity.x;
+
+        // Check if the player is on the ground and if the jump button is pressed
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGround)
+            {
+                currentVelocity.y = velocity.y;
+                jumpTimer = 0.0f;
+                rigidBodyPlayer.gravityScale = jumpGravityScale;
+            }
+        }
+
+        else if (jumpTimer < jumpMaxDuration)
+        {
+            jumpTimer += Time.deltaTime;
+            
+            // If the jump button is still pressed, apply jump gravity
+            if (Input.GetButton("Jump"))
+            {
+                rigidBodyPlayer.gravityScale = Mathf.Lerp(jumpGravityScale, originalGravity, jumpTimer / jumpMaxDuration);
+            }
+
+            // If the jump button is released, reset the jump timer and gravity scale
+            else
+            {
+                jumpTimer = jumpMaxDuration;
+                rigidBodyPlayer.gravityScale = originalGravity;
+            }
+        }
+
+        // If the player is on the ground, reset the jump timer and gravity scale
+        else
+        {
+            rigidBodyPlayer.gravityScale = originalGravity;
+        }
+        
+        rigidBodyPlayer.linearVelocity = currentVelocity;
+    }
+
+    // Draw gizmos in the editor to visualize the check points
+    void OnDrawGizmos()
+    {
+        if (groundCheck == null)
+        {
+            return;
+        }
+
+        if (isGround)
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.yellow;
+        }
+
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
