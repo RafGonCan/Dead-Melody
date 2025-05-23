5cb43699f2d0b20fe7e71c4dbb6fad8334f18c9d
Zuha Rosario <zuhalourenco@gmail.com>
2025-05-23 15:46:58 +0100

    Working on death

--- Game/Assets/Scripts/Level Manager/Mud.cs
+++ Game/Assets/Scripts/Level Manager/Mud.cs
@@ -1,16 +1,77 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Mud : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField, Range(0.0f, 60.0f)] private float delay;
+
+    [SerializeField] private ParticleSystem deathParticle;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private SoundManager soundManager;
+
+    private PlayerMovement playerMovement;
+
+    private bool collided = false;
+
+    private void Start()
     {
-        // Check if the player collided with the mud
-        if (collision.gameObject.GetComponent<PlayerMovement>()) 
+        // Find the SoundManager in the scene
+        soundManager = FindAnyObjectByType<SoundManager>();
+        playerMovement = FindAnyObjectByType<PlayerMovement>();
+        deathParticle = GetComponent<ParticleSystem>();   
+        spriteRenderer = GetComponent<SpriteRenderer>();     
+    }
+
+    private void Update()
+    {
+
+        if (collided)
         {
-            Debug.Log("Collision with player");
+            delay -= Time.deltaTime;
+        }
+        if (delay <= 0.0f && collided)
+        {
+            delay = 0.0f;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        soundManager.SFXPlay(0);
+
+        deathParticle.Play();
+
+        spriteRenderer.enabled = false;
+
+        playerMovement.enabled = false;
+
+        collided = true;
+        /*
+        if (delay <= 0.0f && collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            Debug.Log("Collision with player");
+            delay = 0.0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }*/
+
+        /*
+        // Check if the player collided with the mud
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            Debug.Log("Collision with player");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //StartCoroutine(RestartLevel());
+        }*/
+    }
+    /*
+    private IEnumerator RestartLevel() //Restart level after a delay
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    */
 }
